9058722ccd2a720f6d8145aaff4757b167728830
ameliav <amelia.vance@associates.cisa.dhs.gov>
2021-10-18 15:43:19 -0400

    Add back int() calls in time.py

--- src/pca_report_library/utility/time.py
+++ src/pca_report_library/utility/time.py
@@ -52,9 +52,11 @@ def format_timedelta_to_HHMMSS(in_time_delta):
     """Convert time delta to HH:MM:SS."""
     hours, remainder = divmod(in_time_delta, 3600)
     minutes, seconds = divmod(remainder, 60)
-
+    hours = int(hours)
+    minutes = int(minutes)
+    seconds = int(seconds)
     if minutes < 10:
         minutes = "0{}".format(minutes)
     if seconds < 10:
         seconds = "0{}".format(seconds)
-    return "{}:{}:{}".format(hours, minutes, seconds)
+    return "{}:{}:{}".format(int(hours), int(minutes), int(seconds))
